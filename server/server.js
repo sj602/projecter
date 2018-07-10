a3aff7e768f15f545e85157ef58b5e289ceacec9
sj602 <tjswls602@gmail.com>
2018-07-10 13:40:25 +0900

    heroku

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '..', '/client/static')));
+app.use(express.static(path.join(__dirname, '..', '/dist')));
 
 // DB config
 mongoose.connect('mongodb://localhost:27017/admin', { dbName: 'projecter' })
